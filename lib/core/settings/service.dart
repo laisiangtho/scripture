9e97bf35aaebaeb6454f024ae35fc03c18939c84
Khen Solomon Lethil <khensolomon@gmail.com>
2021-12-05 09:58:00 +0100

    package:lidea/intl.dart

--- lib/core/settings/service.dart
+++ lib/core/settings/service.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:lidea/intl.dart';
 
 // import 'package:bible/core.dart';
 import 'package:bible/type.dart';
