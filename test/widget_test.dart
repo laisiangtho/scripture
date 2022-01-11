1ca22e77e1c2ca3a30b1ed47dc85f71c65effa85
Khen Solomon Lethil <khensolomon@gmail.com>
2022-01-11 15:49:08 +0100

    Update gitignore & add widget_test
    name: scripture

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,8 +11,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main() {
-  group('MyWidget', () {
+import 'package:scripture/main.dart';
+import 'package:scripture/core/main.dart';
+
+void main() async {
+  final core = Core();
+  await core.ensureInitialized();
+
+  group('MyWidgets', () {
+    testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(LaiSiangtho(core: core));
+
+      // Verify that our counter starts at 0.
+      expect(find.text('0'), findsOneWidget);
+      expect(find.text('1'), findsNothing);
+
+      // Tap the '+' icon and trigger a frame.
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+
+      // Verify that our counter has incremented.
+      expect(find.text('0'), findsNothing);
+      expect(find.text('1'), findsOneWidget);
+    });
+
     testWidgets('should display a string of text', (WidgetTester tester) async {
       // Define a Widget
       const myWidget = MaterialApp(
