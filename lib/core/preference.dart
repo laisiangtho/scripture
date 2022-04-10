9db1c2b8904150e52489b0c03b40cf53732745c1
Khen Solomon Lethil <khensolomon@gmail.com>
2022-04-10 09:36:37 +0200

    Preference extends UnitController
    change to library

--- lib/core/preference.dart
+++ lib/core/preference.dart
@@ -1,38 +1,24 @@
-part of 'main.dart';
+part of data.core;
 
-class Preference extends UnitController {
-  final Collection _collection;
-
-  Preference(this._collection);
+class Preference extends ClusterController {
+  Preference(Collection docket) : super(docket);
 
   /// `AppLocalizations.of(context)!`;
-  AppLocalizations get text {
-    return AppLocalizations.of(context)!;
-  }
+  AppLocalizations get text => AppLocalizations.of(context)!;
 
-  List<Locale> get supportedLocales {
-    return AppLocalizations.supportedLocales;
-  }
+  List<Locale> get supportedLocales => AppLocalizations.supportedLocales;
 
-  SettingType get _setting => _collection.setting;
+  late final Iterable<LocalizationsDelegate<dynamic>> localeDelegates = const [
+    AppLocalizations.delegate,
+    GlobalMaterialLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+  ];
 
-  @override
-  int get mode => _setting.mode;
-
-  @override
-  Locale get locale {
-    final lang = _setting.locale;
-    return Locale(lang.isEmpty ? Intl.systemLocale : lang, '');
-  }
-
-  @override
-  Future<void> saveMode(ThemeMode newMode) async {
-    final mode = ThemeMode.values.indexOf(newMode);
-    await _collection.settingUpdate(_setting.copyWith(mode: mode));
-  }
-
-  @override
-  Future<void> saveLocale(Locale newLocale) async {
-    await _collection.settingUpdate(_setting.copyWith(locale: newLocale.languageCode));
-  }
+  /// English, Norwegian, Myanmar
+  late final Iterable<Locale> localeSupports = const [
+    Locale('en', 'GB'),
+    Locale('no', 'NO'),
+    Locale('my', '')
+  ];
 }
