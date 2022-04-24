2ac322a915c5ae55128ac4b135bf67d4ed87a8ad
Khen Solomon Lethil <khensolomon@gmail.com>
2022-04-24 14:41:18 +0200

    add UserIcon

--- lib/view/launch/home/bar.dart
+++ lib/view/launch/home/bar.dart
@@ -27,84 +27,15 @@ mixin _Bar on _State {
           child: WidgetMark(
             child: Selector<Authentication, bool>(
               selector: (_, e) => e.hasUser,
-              builder: userPhoto,
+              builder: (BuildContext _, bool hasUser, Widget? child) {
+                return UserIcon(authenticate: authenticate);
+              },
             ),
           ),
           message: preference.text.option(true),
           onPressed: () => core.navigate(to: '/user'),
         ),
       ],
-    );
-  }
-
-  Widget userPhoto(BuildContext _, bool hasUser, Widget? child) {
-    final user = authenticate.user;
-    if (user != null) {
-      if (authenticate.userPhotoURL != null) {
-        return CircleAvatar(
-          radius: 15,
-          child: ClipOval(
-            child: Material(
-              // child: Image.network(
-              //   authenticate.userPhotoURL!,
-              //   fit: BoxFit.cover,
-              // ),
-              child: CachedNetworkImage(
-                placeholder: (context, url) {
-                  return const Padding(
-                    padding: EdgeInsets.all(3),
-                    child: Icon(
-                      Icons.face_retouching_natural_rounded,
-                      size: 25,
-                    ),
-                  );
-                },
-                imageUrl: authenticate.userPhotoURL!,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-        );
-      }
-      return ClipOval(
-        child: Material(
-          elevation: 10,
-          shape: CircleBorder(
-            side: BorderSide(
-              color: Theme.of(context).backgroundColor,
-              width: .5,
-            ),
-          ),
-          shadowColor: Theme.of(context).primaryColor,
-          child: const Padding(
-            padding: EdgeInsets.all(3),
-            child: Icon(
-              Icons.face_retouching_natural_rounded,
-              // size: 25,
-            ),
-          ),
-        ),
-      );
-    }
-
-    return ClipOval(
-      child: Material(
-        elevation: 30,
-        shape: CircleBorder(
-          side: BorderSide(
-            color: Theme.of(context).backgroundColor,
-            width: .7,
-          ),
-        ),
-        shadowColor: Theme.of(context).primaryColor,
-        child: const Padding(
-          padding: EdgeInsets.all(3),
-          child: Icon(
-            Icons.face,
-            size: 25,
-          ),
-        ),
-      ),
     );
   }
 }
