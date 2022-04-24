638e3b3b4547b05604afd3e726bf01177f44072f
Khen Solomon Lethil <khensolomon@gmail.com>
2022-04-24 14:41:10 +0200

    remove cached_network_image

--- lib/view/launch/home/main.dart
+++ lib/view/launch/home/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'package:lidea/provider.dart';
 import 'package:lidea/hive.dart';
-import 'package:lidea/cached_network_image.dart';
 import 'package:lidea/view/main.dart';
 import 'package:lidea/icon.dart';
 
