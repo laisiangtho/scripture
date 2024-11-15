2b98a600a42c03b1d9799b3e43f5bf66a3a4cc2a
Khen Solomon Lethil <khensolomon@gmail.com>
2024-11-15 20:36:04 +0100

    Add custom Buttons

--- lib/components/buttons.dart
+++ lib/components/buttons.dart
@@ -7,3 +7,65 @@ part of 'main.dart';
 // class TmpViewButtons extends ViewButtons {
 //   const TmpViewButtons({super.key, String label = 'tmp', required super.child});
 // }
+
+/// Action buttons
+class Buttons extends StatefulWidget {
+  const Buttons({
+    super.key,
+    this.type = '',
+  });
+
+  /// ...is extended from  `OptionButtons`.`back` which is stateless
+  const Buttons.back({super.key}) : type = 'back';
+
+  /// ...is extended from  `OptionButtons`.`backOrMenu` which is stateless
+  const Buttons.backOrMenu({super.key}) : type = 'backOrMenu';
+
+  /// ...is extended from  `OptionButtons`.`backOrCancel` which is stateless
+  const Buttons.backOrCancel({super.key}) : type = 'backOrCancel';
+
+  const Buttons.search({super.key}) : type = 'search';
+  final String type;
+
+  @override
+  State<Buttons> createState() => _ButtonsState();
+}
+
+class _ButtonsState extends CommonStates<Buttons> {
+  late final String type = widget.type;
+
+  @override
+  Widget build(BuildContext context) {
+    if (type == 'backOrMenu') {
+      // if (context.adaptive.isMobile) {
+      //   return const Buttons.back();
+      // }
+      return OptionButtons.backOrMenu(
+        menu: app.menu,
+        back: lang.back,
+        onPressed: app.menuToggle,
+      );
+    }
+    if (type == 'backOrCancel') {
+      return OptionButtons.backOrCancel(
+        back: lang.back,
+      );
+    }
+    if (type == 'back') {
+      return OptionButtons.back(
+        label: lang.back,
+      );
+    }
+    if (type == 'search') {
+      return OptionButtons.icon(
+        // icon: Icons.search_rounded,
+        icon: LideaIcon.search,
+        iconSize: 20,
+        onPressed: () {
+          context.push('/search', extra: {'focus': true, 'keyword': data.searchQuery});
+        },
+      );
+    }
+    return const SizedBox();
+  }
+}
